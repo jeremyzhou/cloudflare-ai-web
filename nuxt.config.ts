bfe0178c53b5fe721dfada45c515ea3ef60d48cc
byebyebye <jeremyzhou_2001@sina.com>
2024-09-19 13:19:31 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,15 +14,15 @@ export default defineNuxtConfig({
     },
     app: {
         head: {
-            title: 'CF AI Web',
+            title: '慧医小助手',
             meta: [
                 {
                     name: 'keywords',
-                    content: 'CF AI Web, AI, Cloudflare Workers, ChatGPT, GeminiPro, Google Generative AI'
+                    content: 'A wisdom doctor AI assistant, AI, GeminiPro, A wisdom doctor AI assistant by ZhouZiHao'
                 },
                 {
                     name: 'description',
-                    content: 'Integrated web platform supporting GeminiPro/Cloudflare Workers AI/ChatGPT by Jazee6'
+                    content: 'Integrated web platform supporting GeminiPro/A wisdom doctor AI assistant by ZhouZiHao'
                 }
             ],
             link: [
