8363a32df58373b7896b0cfb4109229f446ced65
byebyebye <jeremyzhou_2001@sina.com>
2024-09-19 18:00:07 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     },
     app: {
         head: {
-            title: '慧医小助手',
+            title: '慧医小助手 design by 周子皓',
             meta: [
                 {
                     name: 'keywords',
