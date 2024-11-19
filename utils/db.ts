b5330c2e93f6d5288f994f4fb93f7c4d257d9364
byebyebye <jeremyzhou_2001@sina.com>
2024-11-19 16:44:53 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -142,4 +142,4 @@ export const imageGenModels: Model[] = [{
     type: 'text-to-image'
 }]
 
-export const models: Model[] = [...uniModals, ...textGenModels, ...imageGenModels]
+export const models: Model[] = [...imageGenModels, ...uniModals]
