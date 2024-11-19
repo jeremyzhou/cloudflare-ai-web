2c630766ff5a78b2026d2fc557a2fac708f61093
byebyebye <jeremyzhou_2001@sina.com>
2024-11-19 16:22:17 +0800

    Update ChatInput.vue

--- components/ChatInput.vue
+++ components/ChatInput.vue
@@ -157,8 +157,20 @@ function handleButtonClick(text: string) {
                @click="handleButtonClick('假如你是一位画家，请画出“春天的萌芽”！')">
         假如你是一位画家，请画出“春天的萌芽”！
       </UButton>
+      <UButton color="white" class="m-1" icon="i-arrow-long-left-16-solid"
+               @click="handleButtonClick('假如你是一位画家，请画出“夏天的繁茂”！')">
+        假如你是一位画家，请画出“夏天的繁茂”！
+      </UButton>
+      <UButton color="white" class="m-1" icon="i-arrow-long-left-16-solid"
+               @click="handleButtonClick('假如你是一位画家，请画出“秋天的收获”！')">
+        假如你是一位画家，请画出“秋天的收获”！
+      </UButton>
+       <UButton color="white" class="m-1" icon="i-arrow-long-left-16-solid"
+               @click="handleButtonClick('假如你是一位画家，请画出“冬天的休眠“！')">
+        假如你是一位画家，请画出“冬天的休眠“！
+      </UButton>
     </div>
-
+<!--
     <div class="flex items-end justify-end">
       <UButton color="white" class="m-1" icon="i-arrow-long-left-16-solid"
                @click="handleButtonClick('假如你是一位画家，请画出“夏天的繁茂”！')">
@@ -179,6 +191,6 @@ function handleButtonClick(text: string) {
         假如你是一位画家，请画出“冬天的休眠“！
       </UButton>
     </div>
-    
+-->    
   </div>
 </template>
