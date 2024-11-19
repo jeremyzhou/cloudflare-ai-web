224b2a062cbe589043329e41c215afc4f2be3921
byebyebye <jeremyzhou_2001@sina.com>
2024-11-19 15:14:29 +0800

    Update Sidebar.vue

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {useGlobalState} from "~/utils/store";
 
-//const {openAside, openSettings} = useGlobalState()
+const {openAside, openSettings} = useGlobalState()
 const {openSettings} = useGlobalState()
 
 defineProps<{
@@ -32,12 +32,10 @@ defineProps<{
     </ol>
     -->
     <div class="flex my-1">
-      <!-- 
       <IButton name="i-heroicons-cog-8-tooth" @click="openSettings=!openSettings"/>
       <UButton class="ml-auto" variant="soft" @click="handleNewChat">
         {{ $t('new_chat') }} +
       </UButton>
-      -->
     </div>
   </aside>
 </template>
