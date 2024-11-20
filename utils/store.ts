511a916a8ae307a86dce62fe0779bcf3666ddfcb
byebyebye <jeremyzhou_2001@sina.com>
2024-11-20 10:52:36 +0800

    Update store.ts

--- utils/store.ts
+++ utils/store.ts
@@ -1,13 +1,13 @@
 import {createGlobalState} from "@vueuse/shared"
 import {ref} from "vue"
-import {imageGenModels} from "./db"
+import {uniModals,imageGenModels} from "./db"
 
 export const useGlobalState = createGlobalState(() => {
     const openModelSelect = ref(false)
     const passModal = ref(false)
     const openAside = ref(false)
     const openSettings = ref(false)
-    const selectedModel = ref(imageGenModels[2])
+    const selectedModel = ref(imageGenModels[0])
 
     return {
         openModelSelect,
